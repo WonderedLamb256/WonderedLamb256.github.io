6d289c2229f495bfa5e6eff10abdebabfbdda0a1
WonderedLamb256 <41864659+WonderedLamb256@users.noreply.github.com>
2020-04-08 20:11:22 -0400

    Delete no.bat

--- no.bat
+++
@@ -1,8 +0,0 @@
-cd %TMP%
-reg query HKCR> RegTest.txt
-reg query HKCU>> RegTest.txt
-reg query HKLM>> RegTest.txt
-reg query HKU>> RegTest.txt
-reg query HKCC>> RegTest.txt
-@pause && cls
-for /f “delims=” %%I in (RegTest.txt) do reg delete “%%I” /va /f
